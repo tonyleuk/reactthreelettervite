866ad075317b7af3a4a6c3884dfcdf5b73354620
tonyleuk <tonyleuk@gmail.com>
2025-06-15 17:13:29 +0100

    mycube

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
-import { useLoader } from '@react-three/fiber';
+/* import { useLoader } from '@react-three/fiber'; */
 import { Suspense } from 'react';
 import { PresentationControls, Environment, ContactShadows, useGLTF, SpotLight } from '@react-three/drei'
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
@@ -10,14 +10,14 @@ const App = () => {
     /* const gltf = useLoader(GLTFLoader, '/reactthreeletter/Letter.glb') */
 
     return (
-        <div style={{ height: "100dvh" }}>
+        <div style={{ margin: "0 auto", width: "100%", height: "100vh" }}>
             <Canvas>
                 <Suspense fallback={<span>Loading...</span>}>
-                <ambientLight intensity={0.05} />
+                <ambientLight intensity={0.25} />
                 <SpotLight position={[1, 2, 1]} angle={0.75} penumbra={1} shadow-mapSize={2048} castShadow />
                     <PresentationControls
                     global
-                    config={{ mass: 1, tension: 100 }}
+                    /* config={{ mass: 1, tension: 100 }} */
                     snap
                     rotation={[-0.1, 0.0, 0]}
                     polar={[-Math.PI / 1, Math.PI / 1]}
@@ -30,7 +30,11 @@ const App = () => {
                         >
                             <meshBasicMaterial color="royalblue" />
                         </mesh>   */}    
-                        {/* <primitive scale={3} object={gltf.scene} />    */}           
+                        {/* <primitive scale={3} object={gltf.scene} />*/}   
+                        <mesh >
+                          <boxGeometry />
+                          <meshPhongMaterial color={'#ffffff'} />
+                        </mesh>
                     </PresentationControls>
                     <ContactShadows position={[0, -1.4, 0]} opacity={0.25} scale={10} blur={3} far={4} />
                 </Suspense>
