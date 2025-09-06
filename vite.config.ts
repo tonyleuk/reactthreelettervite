f6107f96a476dd2abbcdbe442ee13df210823ff2
tonyleuk <tonyleuk@gmail.com>
2025-09-06 20:41:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   assetsInclude: ['**/*.glb'],
   plugins: [react()],
-  base: "/reactthreelettervite"
+  base: "/" //reactthreelettervite
 })
